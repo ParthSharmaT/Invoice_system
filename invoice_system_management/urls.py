60aadfe03e6856e46266bf5d129fec960d58ab97
ParthSharmaT <parthsharmatanguriya@gmail.com>
2022-08-17 20:50:22 +0530

    added heroku

--- invoice_system_management/urls.py
+++ invoice_system_management/urls.py
@@ -18,8 +18,9 @@ from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('invoice.urls')),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("admin/", admin.site.urls),
+    path("/", include("invoice.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
